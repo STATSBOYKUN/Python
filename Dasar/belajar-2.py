e76ec356d9ed9d9a39766fd3a2ee2b0c8f067d2f
STATSBOYKUN <radenumarhp277@student.uns.ac.id>
2022-07-06 10:31:29 +0700

    Belajar format lain

--- Dasar/belajar-2.py
+++ Dasar/belajar-2.py
@@ -1,3 +1,17 @@
-#belajar string
-angka = 2000
-format_str = f"angka = {angka}"
+#belajar format string
+angka = 200090809.80861
+format_str = f"angka = {angka:+,.2f}"
+print(format_str)
+
+persentase = 0.09
+format_persen = f"persentase = {persentase:.3%}"
+print(format_persen)
+
+#format lain
+angka = 111
+format_lain  = f"biner : {bin(angka)}"
+print(format_lain)
+format_lain = f"octal : {oct(angka)}"
+print(format_lain)
+format_lain = f"hex : {hex(angka)}"
+print(format_lain)
