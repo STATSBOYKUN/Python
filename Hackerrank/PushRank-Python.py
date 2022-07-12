61161f242829ef490b5df1ae4b97923e051b3c98
STATSBOYKUN <radenumarhp277@student.uns.ac.id>
2022-07-12 11:55:28 +0700

    Write a function

--- Hackerrank/PushRank-Python.py
+++ Hackerrank/PushRank-Python.py
@@ -47,9 +47,16 @@ if __name__ == '__main__':
 def is_leap(year):
     leap = False
     
-    if year%4 == 0:
-        leap = True
-    
+    if (year%4) == 0:
+        if (year%100 == 0):
+            if (year%400 == 0):
+                leap = True
+            else:
+                leap = False
+        else:
+            leap = True
+        
+        
     return leap
 
 year = int(raw_input())
