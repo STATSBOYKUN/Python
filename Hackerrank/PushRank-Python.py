8f678f0db3e4c31cdec64677df0268382daead88
STATSBOYKUN <radenumarhp277@student.uns.ac.id>
2022-07-12 13:05:00 +0700

    Classes: Dealing with Complex Numbers

--- Hackerrank/PushRank-Python.py
+++ Hackerrank/PushRank-Python.py
@@ -66,3 +66,46 @@ print is_leap(year)
 if __name__ == '__main__':
     n = int(input())
     print("".join(str(i) for i in range(1,n+1)))
+
+# Classes: Dealing with Complex Numbers
+import math
+class Complex(complex):
+    def __init__(self, real, imaginary):
+        self.real = real
+        self.imaginary = imaginary
+        
+    def __add__(self, no):
+        return Complex(complex.__add__(self, no))
+
+    def __sub__(self, no):
+        return Complex(complex.__sub__(self, no))
+
+    def __mul__(self, no):
+        return Complex(complex.__mul__(self, no))
+
+    def __truediv__(self, no):
+        return Complex(complex.__truediv__(self, no))
+
+    def mod(self):
+        return Complex(complex.__abs__(self))
+    
+    def __str__(self):
+        if self.imaginary == 0:
+            result = "%.2f+0.00i" % (self.real)
+        elif self.real == 0:
+            if self.imaginary >= 0:
+                result = "0.00+%.2fi" % (self.imaginary)
+            else:
+                result = "0.00-%.2fi" % (abs(self.imaginary))
+        elif self.imaginary > 0:
+            result = "%.2f+%.2fi" % (self.real, self.imaginary)
+        else:
+            result = "%.2f-%.2fi" % (self.real, abs(self.imaginary))
+        return result
+
+if __name__ == '__main__':
+    c = map(float, input().split())
+    d = map(float, input().split())
+    x = Complex(*c)
+    y = Complex(*d)
+    print(*map(str, [x+y, x-y, x*y, x/y, x.mod(), y.mod()]), sep='\n')
