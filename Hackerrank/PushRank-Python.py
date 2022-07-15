25f66338baaaf595ff365f7fafcb4038245b62e3
STATSBOYKUN <radenumarhp277@student.uns.ac.id>
2022-07-15 19:23:02 +0700

    What's Your Name?

--- Hackerrank/PushRank-Python.py
+++ Hackerrank/PushRank-Python.py
@@ -241,3 +241,11 @@ def split_and_join(line):
 line = input()
 result = split_and_join(line)
 print(result)
+
+# What's Your Name?
+def print_full_name(first, last):
+   print("Hello {} {}! You just delved into python.".format(first,last))
+   
+first_name = input()
+last_name = input()
+print_full_name(first_name, last_name)
--- coba.py
+++ coba.py
@@ -1,7 +1,6 @@
-def split_and_join(line):
-   word_temp = line.split(" ")
-   return "-".join(word_temp)
-
-line = input()
-result = split_and_join(line)
-print(result)
+def print_full_name(first, last):
+   print("Hello {} {}! You just delved into python.".format(first,last))
+   
+first_name = input()
+last_name = input()
+print_full_name(first_name, last_name)
