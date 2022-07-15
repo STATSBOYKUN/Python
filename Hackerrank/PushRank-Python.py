b633492931a9a41124d90fc5977689bf5ae10dc9
STATSBOYKUN <radenumarhp277@student.uns.ac.id>
2022-07-15 18:28:21 +0700

    Lists

--- Hackerrank/PushRank-Python.py
+++ Hackerrank/PushRank-Python.py
@@ -186,3 +186,25 @@ for i in list_temp:
       count_word += 1
 
 print("{0:.3f}".format(count_word/len(list_temp)))
+
+N = int(input())
+arr_temp = []      
+
+# Lists
+for _ in range(N):
+   command_temp = input().split()
+   argument_temp = ["insert", "remove", "append", "pop", "reverse", "sort", "print"]
+   if str(command_temp[0]) == argument_temp[0]:
+      arr_temp.insert(int(command_temp[1]), int(command_temp[2]))
+   elif str(command_temp[0]) == argument_temp[1]:
+      arr_temp.remove(int(command_temp[1]))
+   elif str(command_temp[0]) == argument_temp[2]:
+      arr_temp.append(int(command_temp[1]))
+   elif str(command_temp[0]) == argument_temp[3]:
+      arr_temp.pop()
+   elif str(command_temp[0]) == argument_temp[4]:
+      arr_temp.reverse()
+   elif str(command_temp[0]) == argument_temp[5]:
+      arr_temp.sort()
+   elif str(command_temp[0]) == argument_temp[6]:
+      print(arr_temp)
--- coba.py
+++ coba.py
@@ -1,15 +1,20 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-from itertools import combinations
+N = int(input())
+arr_temp = []      
 
-n = input()
-word_temp = input().split()
-r = int(input())
-
-list_temp = list(combinations(''.join(word_temp),r))
-
-count_word = 0
-for i in list_temp:
-   if 'a' in i:
-      count_word += 1
-
-print("{0:.3f}".format(count_word/len(list_temp)))
+for _ in range(N):
+   command_temp = input().split()
+   argument_temp = ["insert", "remove", "append", "pop", "reverse", "sort", "print"]
+   if str(command_temp[0]) == argument_temp[0]:
+      arr_temp.insert(int(command_temp[1]), int(command_temp[2]))
+   elif str(command_temp[0]) == argument_temp[1]:
+      arr_temp.remove(int(command_temp[1]))
+   elif str(command_temp[0]) == argument_temp[2]:
+      arr_temp.append(int(command_temp[1]))
+   elif str(command_temp[0]) == argument_temp[3]:
+      arr_temp.pop()
+   elif str(command_temp[0]) == argument_temp[4]:
+      arr_temp.reverse()
+   elif str(command_temp[0]) == argument_temp[5]:
+      arr_temp.sort()
+   elif str(command_temp[0]) == argument_temp[6]:
+      print(arr_temp)
