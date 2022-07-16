56f9c143889bcc060f6df241f81824489a03692b
STATSBOYKUN <radenumarhp277@student.uns.ac.id>
2022-07-16 11:29:18 +0700

    String Formatting

--- coba.py
+++ coba.py
@@ -1,13 +1,7 @@
-n = input().split()
-a = "-"
-b = ".|."
-range_temp = [int(i) for i in range(1, int(n[0])) if (i%2) != 0]
+def print_formatted(number):
+    width = len(bin(number)) - 2
+    for i in range(1, number + 1):
+        print(str(i).rjust(width), oct(i)[2:].rjust(width), hex(i)[2:].upper().rjust(width), bin(i)[2:].rjust(width))
 
-for i in range_temp:
-    print((i*b).center(int(n[1]),a))
-
-print("WELCOME".center(int(n[1]),a))
-
-range_temp.reverse()
-for i in range_temp:
-    print((i*b).center(int(n[1]),a))
+n = int(input())
+print_formatted(n)
