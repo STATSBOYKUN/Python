e6981e8ead3643ac846041040ae96d7b090775e3
STATSBOYKUN <radenumarhp277@student.uns.ac.id>
2022-07-16 11:24:02 +0700

    Designer Door Mat

--- Hackerrank/PushRank-Python.py
+++ Hackerrank/PushRank-Python.py
@@ -320,3 +320,18 @@ def wrap(string, max_width):
 string, max_width = input(), int(input())
 result = wrap(string, max_width)
 print(result)
+
+# Designer Door Mat
+n = input().split()
+a = "-"
+b = ".|."
+range_temp = [int(i) for i in range(1, int(n[0])) if (i%2) != 0]
+
+for i in range_temp:
+    print((i*b).center(int(n[1]),a))
+
+print("WELCOME".center(int(n[1]),a))
+
+range_temp.reverse()
+for i in range_temp:
+    print((i*b).center(int(n[1]),a))
--- coba.py
+++ coba.py
@@ -1,8 +1,13 @@
-import textwrap
+n = input().split()
+a = "-"
+b = ".|."
+range_temp = [int(i) for i in range(1, int(n[0])) if (i%2) != 0]
 
-def wrap(string, max_width):
-    return textwrap.fill(string, max_width)
+for i in range_temp:
+    print((i*b).center(int(n[1]),a))
 
-string, max_width = input(), int(input())
-result = wrap(string, max_width)
-print(result)
+print("WELCOME".center(int(n[1]),a))
+
+range_temp.reverse()
+for i in range_temp:
+    print((i*b).center(int(n[1]),a))
