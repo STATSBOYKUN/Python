9d2979adf905d073e9fa0e86ef848d97c5b373e8
STATSBOYKUN <radenumarhp277@student.uns.ac.id>
2022-07-16 10:16:33 +0700

    Day 4: Class vs. Instance

--- Hackerrank/30-Days-Challenge.py
+++ Hackerrank/30-Days-Challenge.py
@@ -71,4 +71,33 @@ if (n%2) == 0:
     elif n in range(6,21):
         print("Weird")
 else:
-    print("Weird") 
+    print("Weird")
+
+# Day 4: Class vs. Instance
+class Person:
+    def __init__(self,initialAge):
+        self.age = initialAge
+    def amIOld(self):
+        if self.age < 0:
+            print("Age is not valid, setting age to 0.")
+            self.age = 0
+            print("You are young.")
+            self.age = 3
+        elif self.age < 13:
+            print("You are young.")
+        elif self.age < 18:
+            print("You are a teenager.")
+        else:
+            print("You are old.")
+    def yearPasses(self):
+        self.age += 1
+
+t = int(input())
+for i in range(0, t):
+    age = int(input())         
+    p = Person(age)  
+    p.amIOld()
+    for j in range(0, 3):
+        p.yearPasses()       
+    p.amIOld()
+    print("")
