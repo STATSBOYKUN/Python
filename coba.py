7db9b4854ee42bb9a88d5c2dd63fa7c3c54a80b3
STATSBOYKUN <radenumarhp277@student.uns.ac.id>
2022-08-03 22:14:13 +0700

    Day 12: Inheritance

--- coba.py
+++ coba.py
@@ -1,16 +1,37 @@
-list_temp = []
-for i in range(6):
-    list_temp.append(input().split())
+class Person:
+   	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
 
-sum_temp = 0
-list_sum = []
-for i in range(4):
-    for j in range(4):
-        sum_temp += (int(list_temp[i][j]) + int(list_temp[i][j+1]) + int(list_temp[i][j+2]))
-        sum_temp += (int(list_temp[i+1][j+1]))
-        sum_temp += (int(list_temp[i+2][j]) + int(list_temp[i+2][j+1]) + int(list_temp[i+2][j+2]))
-        
-        list_sum.append(sum_temp)
-        sum_temp = 0
+class Student(Person):
+    def __init__(self, firstName, lastName, idNumber, scores):
+        super().__init__(firstName, lastName, idNumber)
+        self.scores = scores
+    def calculate(self):
+        avg = sum(self.scores)/len(self.scores)
+        if avg >= 90:
+            return "O"
+        elif avg >= 80:
+            return "E"
+        elif avg >= 70:
+            return "A"
+        elif avg >= 55:
+            return "P"
+        elif avg >= 40:
+            return "D"
+        else:
+            return "T"
 
-print(max(list_sum))
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
